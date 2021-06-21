8b742432e9e74a922e63ace74542dce0a73f73a3
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-20 20:35:31 -0500

    [sm] testing out some svg to string puts

--- convert.rb
+++ convert.rb
@@ -4,4 +4,6 @@ File.foreach("in/map.svg") { |line| puts line }
 File.foreach("in/text.txt") { |line| puts line }
 file_data = file.read
 file_data = file.readlines.map(&:chomp)
+puts file.to_s
+puts file_data.to_s
 # print file_data
