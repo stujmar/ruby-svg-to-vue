06c39d1e48fce2bd0803e4bd66d90539f060bc4d
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-20 20:38:04 -0500

    [sm] was able to see dummy svg data via a foreach puts line

--- convert.rb
+++ convert.rb
@@ -1,9 +1,12 @@
-puts 'Hello World'
 file = File.open("in/map.svg")
 File.foreach("in/map.svg") { |line| puts line }
-File.foreach("in/text.txt") { |line| puts line }
+# File.foreach("in/text.txt") { |line| puts line }
 file_data = file.read
 file_data = file.readlines.map(&:chomp)
-puts file.to_s
-puts file_data.to_s
+puts 'Print File'
+puts file
+puts 'Print File Data'
+puts file_data
+# puts file.to_s
+# puts file_data.to_s
 # print file_data
