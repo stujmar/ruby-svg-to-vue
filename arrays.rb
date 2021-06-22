0196b11138bda40d1e25e708ccde0f32bdcb3482
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-21 20:09:57 -0500

    [sm] went over instantiating and accessing ruby arrays in a variety of different ways

--- arrays.rb
+++ arrays.rb
@@ -1,3 +1,48 @@
-# Arrays can hold a mixture of difference data types.
+# Arrays can hold a mixture of difference data types. [1, "two", 3.0]
 my_array = [1, "two", 3.0]
 print my_array
+
+# Empty arrays can be generated with a new method on the array class. []
+empty_array = Array.new
+print empty_array
+
+# Empty arrays of a certain length. [nil, nil, nil]
+three_array = Array.new(3)
+print three_array
+
+# Empty array of a certain length and value.
+true_array = Array.new(3, true)
+print true_array
+
+puts # new line
+
+# Create a new array of empty hashes
+hash_array = Array.new(3) { Hash.new }
+print hash_array
+
+# Create a new array and use an interator.
+num_array = Array.new(3) {|i| i.to_s }
+print num_array
+
+puts # new line
+
+# Use this method to create multidimensional arrays.
+empty_table = Array.new(3) { Array.new(3) }
+print empty_table
+
+puts # new line
+
+# There is also the array method.
+method_array = Array({:a => "a", :b => "b"}) 
+print method_array
+
+# Accessing arrays.
+access_array = [1, 2, 3, 4, 5, 6]
+access_array[2] #=> 3
+access_array[100] #=> nil
+access_array[-3] #=> 4 (counts from the end)
+access_array[1..4] #=> [2,3,4,5] from one index to another including both indices.
+access_array[1..-3] #=> [2,3,4] from start index to backcount position.
+
+# Or use the at method on the array
+print access_array.at(5)
