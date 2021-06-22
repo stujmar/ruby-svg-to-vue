ba3332a4a0967d24c8958280b13e63d629ef9e74
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-21 19:03:56 -0500

    [sm] got a foreach to print each line int he file

--- read_text.rb
+++ read_text.rb
@@ -1,4 +1,15 @@
 file = File.open("in/text.txt")
-puts file
+# puts file
+
 file_data = file.read
-puts file_data
+# puts file_data
+
+file_lines = file.readlines.map(&:chomp)
+# puts file_lines
+
+# Type Error
+# file_split = file.read("in/text.txt").split
+# puts file_split
+
+line = []
+File.foreach("in/text.txt") { |line| puts line }
