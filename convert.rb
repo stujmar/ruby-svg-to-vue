bb441f88641af25363697c2d7384ea92e28ad4cc
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-22 22:10:47 -0500

    [sm] was able to check line by line for a regex match in the svg document

--- convert.rb
+++ convert.rb
@@ -3,14 +3,20 @@ File.foreach("in/map.svg") { |line| puts line }
 # File.foreach("in/text.txt") { |line| puts line }
 file_data = file.read
 file_data = file.readlines.map(&:chomp)
-puts 'Print File'
-puts file.to_s
-puts 'Print File Data'
-puts file_data.to_s
+# puts 'Print File'
+# puts file.to_s
+# puts 'Print File Data'
+# puts file_data.to_s
 
 output_array = []
 output_string = "<template>\n"
 File.foreach("in/map.svg") { |line| output_array.append(line) }
+output_array.each_with_index { 
+    |line, index| 
+    if /id="/.match(line)
+        puts "we got a match at #{index}"
+    end
+}
 output_array.each { |line| output_string = output_string + "\t" + line }
 output_string += "</template>"
 File.write("out/map.vue", output_string)
